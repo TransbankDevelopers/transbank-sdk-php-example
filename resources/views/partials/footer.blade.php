ee40f155de99f0937c2ef1fe15781ac750faba73
victor mendoza <victor.mendoza@continuumhq.com>
2024-07-08 11:06:11 -0300

    feat: create footer partial

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="bg-tbk-black-bg tbk-footer">
+    <div class="footer-item">
+        <img src="{{ asset('images/tbk-white.svg') }}" alt="tbk-logo-white" width="{200}" height="{38}" />
+    </div>
+    <span class="footer-item boder-t-gray3">
+        Hecho con amor por Continuum y Transbank.
+    </span>
+</footer>
