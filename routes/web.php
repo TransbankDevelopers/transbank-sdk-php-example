323cf2b9db444d04231d23bc3a30bbcf9bd2d399
victor mendoza <victor.mendoza@continuumhq.com>
2024-07-08 11:01:21 -0300

    feat: add home route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'home')->name('home');
+Route::view('/webpay-plus', 'home')->name('webpay-plus');
+Route::view('/oneclick-mall', 'home')->name('oneclick-mall');
+Route::view('/transaccion-completa', 'home')->name('transaccion-completa');
+Route::view('/patpass-comercio', 'home')->name('patpass');
