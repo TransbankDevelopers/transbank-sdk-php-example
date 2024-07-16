cee7953be2810c7e260c307a1dcafeb5c42eb65d
victor mendoza <ve.mendoza.blanco@gmail.com>
2024-07-16 18:33:44 -0300

    feat: create webpay routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,17 @@
 <?php
 
+use App\Http\Controllers\WebpayController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::view('/', 'home')->name('home');
-Route::view('/webpay-plus', 'home')->name('webpay-plus');
 Route::view('/oneclick-mall', 'home')->name('oneclick-mall');
 Route::view('/transaccion-completa', 'home')->name('transaccion-completa');
 Route::view('/patpass-comercio', 'home')->name('patpass');
+
+Route::prefix('webpay-plus')->name("webpay.")->group(function () {
+    Route::get('/create', [WebpayController::class, 'index'])->name("create");
+    Route::get('/commit', [WebpayController::class, 'commit'])->name("commit");
+    Route::post('/refund', [WebpayController::class, 'refund'])->name("refund");
+    Route::get('/status', [WebpayController::class, 'status'])->name("status");
+});
