b2a71dbffb1002a096a529fd044d697aa83f6687
victor mendoza <ve.mendoza.blanco@gmail.com>
2024-07-16 18:51:35 -0300

    feat: create layout component

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>{{ $title ?? 'Webpay Plus' }}</title>
+    <link rel="stylesheet" href="{{ asset('vendor/scrivo') }}">
+
+</head>
+
+<body>
+    <div class="main-container">
+        @include('partials.header')
+        <div class="body-container">
+
+            <x-menu :active-link="$activeLink" />
+            <div class="body-content">
+                {{ $slot }}
+                @include('partials.channels')
+            </div>
+            <div class="helper-content">
+                <x-navigation :navigation="$navigation" />
+            </div>
+        </div>
+
+        @include('partials.footer')
+    </div>
+    @stack('scripts')
+</body>
+
+</html>
