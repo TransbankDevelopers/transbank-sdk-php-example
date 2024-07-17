c26619070cf6757eed277b548818a174abc3af45
victor mendoza <ve.mendoza.blanco@gmail.com>
2024-07-17 18:07:22 -0300

    refactor: use options for integation

--- app/Http/Controllers/WebpayController.php
+++ app/Http/Controllers/WebpayController.php
@@ -3,49 +3,65 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Transbank\Webpay\WebpayPlus;
 use Transbank\Webpay\WebpayPlus\Transaction;
+use Transbank\Webpay\Options;
+
+
 
 class WebpayController extends Controller
 {
+    const COMMERCE_CODE = "597055555532";
+    const API_KEY = "579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C";
+    private Transaction $transaction;
 
-    public function __construct(){
-        WebpayPlus::configureForTesting();
+    public function __construct()
+    {
+        $option = new Options(self::API_KEY, self::COMMERCE_CODE, Options::ENVIRONMENT_INTEGRATION);
+        $this->transaction = new Transaction($option);
     }
 
-    public function index(){
+    public function index()
+    {
 
-        $cretaTx = [
+        $createTx = [
             'buyOrder' => "O-" . rand(1, 10000),
-            "sessionId"=> "S-" . (string)(rand(1, 10000)),
+            "sessionId" => "S-" . (string)(rand(1, 10000)),
             'returnUrl' => url('/') . '/webpay-plus/commit',
             'amount' => rand(1000, 2000)
         ];
+        // $transaction = Transaction::buildForIntegration($commerceCode, $apiKey);
 
-        $resp = (new Transaction)->create($cretaTx['buyOrder'], $cretaTx['sessionId'], $cretaTx['amount'], $cretaTx['returnUrl']);
-        return view('webpay.create', ["request" => $cretaTx, "respond" => $resp]);
+        $resp = $this->transaction->create($createTx['buyOrder'], $createTx['sessionId'], $createTx['amount'], $createTx['returnUrl']);
+        return view('webpay.create', ["request" => $createTx, "respond" => $resp]);
     }
-    public function commit(Request $request){
+    public function commit(Request $request)
+    {
+
+        //flujo error
+        if ($request->exists("TBK_TOKEN") && $request->exists("token_ws")) {
+            return view('webpay.error', ["request" => $request]);
+        }
+
         //Flujo normal
-        if($request->exists("token_ws")){
-            $resp = (new Transaction)->commit($request["token_ws"]);
+        if ($request->exists("token_ws")) {
+            $resp = $this->transaction->commit($request["token_ws"]);
             return view('webpay.commit', ["resp" => $resp, "token" => $request["token_ws"]]);
         }
 
         //Pago abortadoas
-        if($request->exists("TBK_TOKEN")){
+        if ($request->exists("TBK_TOKEN")) {
             return view('webpay.error', ["request" => $request]);
         }
 
         //Timeout
         return view('webpay.timeout', ["request" => $request]);
-
     }
 
-    public function refund(Request $request){
+    public function refund(Request $request)
+    {
         try {
             $req = $request->except('_token');
-            $resp = (new Transaction)->refund($req["token"], $req["amount"]);
+            $resp = $this->transaction->refund($req["token"], $req["amount"]);
         } catch (\Exception $e) {
             $resp = array(
                 'msg' => $e->getMessage(),
@@ -56,10 +72,10 @@ class WebpayController extends Controller
         return view('webpay.refund', ["resp" => $resp]);
     }
 
-    public function status(Request $request){
-        // $req = $request->except('_token');
+    public function status(Request $request)
+    {
         $token = $request["token"];
-        $resp = (new Transaction)->status($token);
+        $resp = $this->transaction->status($token);
         return view('webpay.status', ["resp" => $resp, "req" => $request]);
     }
 }
