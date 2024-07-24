c0622eed7516a607e9385aae55f5e032817215de
victor mendoza <ve.mendoza.blanco@gmail.com>
2024-07-24 18:01:03 -0300

    refactor: use vite for compile css and js

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <title>Transbank developers</title>
+    <title>Transbank Developers</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
