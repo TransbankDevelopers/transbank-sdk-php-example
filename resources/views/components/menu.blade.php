e92e7361b3f882f6e727f4313e0817d0249236ce
victor mendoza <victor.mendoza@continuumhq.com>
2024-08-01 11:34:12 -0300

    fix: use correct links for webpay mall deferred

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -5,7 +5,7 @@
         'Webpay Plus' => route('webpay.create'),
         'Webpay Plus Diferido' => route('webpay-deferred.create'),
         'Webpay Mall' => route('webpay-mall.create'),
-        'Webpay Mall Diferido' => route('webpay.create'),
+        'Webpay Mall Diferido' => route('webpay-mall-deferred.create'),
     ] as $menuItemName => $url)
             <a href="{{ $url }}" class="tbk-menu-item {{ $activeLink == $menuItemName ? 'active' : '' }}">
                 {{ $menuItemName }}
