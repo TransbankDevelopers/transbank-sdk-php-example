08910ab995b97a6d3deaf0057f25aaf5ae2a7ef3
victor mendoza <ve.mendoza.blanco@gmail.com>
2024-08-22 17:03:04 -0300

    feat: add protection for window.opener

--- resources/views/partials/channels.blade.php
+++ resources/views/partials/channels.blade.php
@@ -5,7 +5,7 @@
             <div class="question-item">
                 <span class="title">Escríbenos por slack</span>
                 <div class="inner-container">
-                    <a href="https://transbank.continuumhq.dev/slack_community" target="_blank">
+                    <a href="https://transbank.continuumhq.dev/slack_community" target="_blank" rel="noopener">
                         <img src={{ asset('images/slack.png') }} alt="slack" width="124" height="32" />
                     </a>
                     <span class="container">Únete a nuestra comunidad de integradores. Nuestro equipo está ahí para
@@ -16,7 +16,7 @@
             <div class="question-item">
                 <span class="title">Envíanos un mensaje</span>
                 <div class="inner-container">
-                    <a href="mailto:soporte@transbank.cl" target="_blank">
+                    <a href="mailto:soporte@transbank.cl" target="_blank" rel="noopener">
                         <img src={{ asset('images/outlook.png') }} alt="outlook" width="32" height="32" />
                     </a>
                     <span class="container">Necesitas resolver algún tipo de incidencia, contáctanos a través de correo
