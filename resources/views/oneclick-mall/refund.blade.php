83cf6d1ddfd078d9365e18a2fd291c1e8e3d31e8
victor mendoza <ve.mendoza.blanco@gmail.com>
2024-08-26 11:52:42 -0300

    feat: add more info about refund

--- resources/views/oneclick-mall/refund.blade.php
+++ resources/views/oneclick-mall/refund.blade.php
@@ -5,7 +5,8 @@
 <x-layout active-link="Oneclick Mall" :navigation="$navigation">
 
     <h1 id="refund">Oneclick Mall - Reembolsar</h1>
-    <p class="mb-32">En esta etapa, tienes la opción de solicitar el reembolso del monto al titular de la tarjeta.
+    <p class="mb-32">
+        En esta etapa, tienes la opción de solicitar el reembolso del monto al titular de la tarjeta.
         Dependiendo del monto y el tiempo transcurrido desde la transacción, este proceso podría resultar en una
         Reversa, Anulación o Anulación Parcial.
     </p>
@@ -23,10 +24,30 @@
     </ul>
 
     <p class="mb-32">
-        Ten en cuenta que si anulas el monto total, puede ser una Reversa o Anulación, dependiendo de ciertas
-        condiciones, o una Anulación parcial si el monto es menor al total. No es posible realizar Anulaciones ni
-        Anulaciones parciales en tarjetas que no sean de crédito. Tampoco es posible realizar reembolsos de compras en
-        cuotas.
+        Las transacciones de Webpay se pueden anular o reversar dadas algunas condiciones. Para cualquiera de éstas
+        operaciones se utiliza el mismo servicio web que discernirá si se realizará una reversa o una anulación.
+
+        Para poder ejecutar una reversa ésta debe ser realizada antes de las 3 horas de efectuada la confirmación por el
+        monto total y en compras con tarjeta de crédito, débito o prepago.
+
+        Una vez pasadas las tres horas, siempre se ejecutará una anulación.
+    </p>
+    <ul class="mb-32">
+        <li>
+            En transacciones con tarjeta de débito o prepago solo es posible anular por el monto total.
+        </li>
+        <li>
+            En transacciones con tarjeta de crédito puedes anular por cualquier monto igual o menor al total de la
+            compra.
+        </li>
+        <li>
+            No se pueden hacer Anulaciones parciales de compras con cuotas.
+        </li>
+    </ul>
+
+    <p class="mb-32">
+        En <a href="https://www.transbankdevelopers.cl/producto/webpay#anulaciones-y-reversas" class="tbk-link">este link
+        </a> podrás ver mayor información sobre las condiciones y casos para anular o reversar transacciones.
     </p>
 
     <x-snippet>
