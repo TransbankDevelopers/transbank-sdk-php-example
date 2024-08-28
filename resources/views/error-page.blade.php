0783d12fc274e9002cb6104f835081089ccf818e
victor mendoza <ve.mendoza.blanco@gmail.com>
2024-08-28 11:37:54 -0300

    feat: add link to github issue

--- resources/views/error-page.blade.php
+++ resources/views/error-page.blade.php
@@ -20,13 +20,16 @@
                 Lo sentimos, pero se ha producido un error durante la integración con el SDK. El SDK
                 devuelve una Exception.
             </p>
+            <x-snippet :content="$error" />
             <p class="mb-32">Si este error persiste y crees que es necesario reportarlo, por favor, hazlo en nuestro
-                repositorio de GitHub.
+                repositorio de <a class="tbk-link" target="_blank" rel="noopener"
+                    href="https://github.com/TransbankDevelopers/transbank-sdk-php-example/issues/new">GitHub.
+                </a>
             </p>
 
-            <x-snippet :content="$error" />
 
-            <a href="{{ route('home') }}" class="tbk-button primary">Volver</a>
+
+            <a href="{{ route('home') }}" class="tbk-button primary mb-32">Volver</a>
         </main>
         @include('partials.footer')
     </div>
