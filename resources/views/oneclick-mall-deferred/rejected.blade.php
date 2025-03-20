c35143cd5499623614d0efa048479b72c5c7fc20
Matias Vasquez <m.vasquezac@gmail.com>
2025-03-20 16:45:32 -0300

    feat: update navigation content on rejected views

--- resources/views/oneclick-mall-deferred/rejected.blade.php
+++ resources/views/oneclick-mall-deferred/rejected.blade.php
@@ -1,21 +1,21 @@
 @php
-    $navigation = ['rejected' => 'Rechazo Bancario', 'example' => 'Ejemplo'];
+$navigation = ['data' => 'Datos recibidos', 'request' => 'Petición', 'response' => 'Respuesta'];
 @endphp
 
 <x-layout active-link="Oneclick Mall Diferido" :navigation="$navigation">
-    <h1 id="rejected">Oneclick Mall Diferido- Rechazo Bancario</h1>
+    <h1>Oneclick Mall Diferido- Rechazo Bancario</h1>
     <p class="mb-32">
         En esta fase, pueden surgir inconvenientes, ya sea con el titular de la tarjeta o a nivel bancario, lo que
         resulta en el estado final de la transacción siendo marcado como "FAILED".
     </p>
 
-    <h2>Paso 1: Datos recibidos</h2>
+    <h2 id="data">Paso 1: Datos recibidos</h2>
     <p class="mb-32">
         Después de finalizar el flujo en el formulario de inscripción, recibirás un GET con la siguiente información:
     </p>
     <x-snippet :content="$token" />
 
-    <h2>Paso 2: Petición de autorización</h2>
+    <h2 id="request">Paso 2: Petición de autorización</h2>
     <p class="mb-32">
         Utiliza el token recibido para finalizar la Inscripción mediante una nueva llamada a Oneclick.
     </p>
@@ -24,7 +24,7 @@
         $resp = $mallInscription->finish($token);
     </x-snippet>
 
-    <h2>Paso 3: Respuesta</h2>
+    <h2 id="response">Paso 3: Respuesta</h2>
     <p class="mb-32">
         Transbank responderá con la siguiente información.
     </p>
