bcdf99c0c7c4bcad39fc23ecbd9b87dcf81ccd86
Matias Vasquez <m.vasquezac@gmail.com>
2025-03-31 17:31:02 -0300

    feat: replace default title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Transbank Developers</title>
+    <title>Transbank SDK</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
 </head>
