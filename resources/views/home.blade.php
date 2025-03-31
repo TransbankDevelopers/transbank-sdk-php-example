e0aa6709cc56a2e1efcab6fbf28c94d023e386c3
Matias Vasquez <m.vasquezac@gmail.com>
2025-03-31 17:30:02 -0300

    feat: use favicon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Transbank Developers</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
 </head>
 
 <body>
