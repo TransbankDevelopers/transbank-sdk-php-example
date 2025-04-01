d5b0f8fb6105e63147ce8e15054a419d1661dcec
mvarlic <mvarlic@gmail.com>
2025-04-01 15:39:53 -0500

    feat: update rejected error view

--- resources/views/error/oneclick/rejected.blade.php
+++ resources/views/error/oneclick/rejected.blade.php
@@ -10,28 +10,7 @@ $navigation = ['rejected' => 'Rechazo Bancario'];
     </p>
 
     <h2 id="data">Paso 1: Datos recibidos</h2>
-    <p class="mb-32">
-        Después de finalizar el flujo en el formulario de inscripción, recibirás un GET con la siguiente información:
-    </p>
-    <x-snippet :content="$token" />
-
-    <h2 id="request">Paso 2: Petición de autorización</h2>
-    <p class="mb-32">
-        Utiliza el token recibido para finalizar la Inscripción mediante una nueva llamada a Oneclick.
-    </p>
-
-    <x-snippet>
-        $resp = $mallInscription->finish($token);
-    </x-snippet>
-
-    <h2 id="response">Paso 3: Respuesta</h2>
-    <p class="mb-32">
-        Transbank responderá con la siguiente información.
-    </p>
-
+    
     <x-snippet :content="$resp" />
-
-
-    <p class="mb-32">En este caso la inscripción fue rechazada, tendrás que repetir el proceso</p>
 
 </x-layout>
