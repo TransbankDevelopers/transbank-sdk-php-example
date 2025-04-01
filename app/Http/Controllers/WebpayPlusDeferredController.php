9490866f4c99ff87a0e9540737dbf5d7c144963e
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2025-04-01 16:07:31 -0300

    feat: implement error page in webpay deferred controller

--- app/Http/Controllers/WebpayPlusDeferredController.php
+++ app/Http/Controllers/WebpayPlusDeferredController.php
@@ -22,37 +22,45 @@ class WebpayPlusDeferredController extends Controller
     public function create()
     {
 
-        $createTx = [
-            'buyOrder' => "O-" . random_int(1, 10000),
-            "sessionId" => "S-" . random_int(1, 10000),
-            'returnUrl' => url('/') . '/webpay-plus-diferido/commit',
-            'amount' => random_int(1000, 2000)
-        ];
+        try {
+            $createTx = [
+                'buyOrder' => "O-" . random_int(1, 10000),
+                "sessionId" => "S-" . random_int(1, 10000),
+                'returnUrl' => url('/') . '/webpay-plus-diferido/commit',
+                'amount' => random_int(1000, 2000)
+            ];
 
+            $resp = $this->transaction->create($createTx['buyOrder'], $createTx['sessionId'], $createTx['amount'], $createTx['returnUrl']);
+        } catch (\Exception $e) {
+            return view('error-page', ["error" => $e->getMessage()]);
+        }
 
-        $resp = $this->transaction->create($createTx['buyOrder'], $createTx['sessionId'], $createTx['amount'], $createTx['returnUrl']);
         return view('webpay-deferred.create', ["request" => $createTx, "respond" => $resp]);
     }
     public function commit(Request $request)
     {
-        //Timeout
-        $view = 'error.webpay.timeout';
-        $data = ["request" => $request, "product" => self::PRODUCT];
+        try {
+            //Timeout
+            $view = 'error.webpay.timeout';
+            $data = ["request" => $request, "product" => self::PRODUCT];
 
-        //flujo error
-        if ($request->exists("TBK_TOKEN") && $request->exists("token_ws")) {
-            $view = 'error.webpay.form-error';
-        }
-        //Pago abortado
-        elseif ($request->exists("TBK_TOKEN")) {
-            $view = 'error.webpay.aborted';
-            $data["resp"] = $this->transaction->status($request["TBK_TOKEN"]);
-        }
-        //Flujo normal
-        elseif ($request->exists("token_ws")) {
-            $resp = $this->transaction->commit($request["token_ws"]);
-            $view = 'webpay-deferred.commit';
-            $data = ["resp" => $resp, "token" => $request["token_ws"]];
+            //flujo error
+            if ($request->exists("TBK_TOKEN") && $request->exists("token_ws")) {
+                $view = 'error.webpay.form-error';
+            }
+            //Pago abortado
+            elseif ($request->exists("TBK_TOKEN")) {
+                $view = 'error.webpay.aborted';
+                $data["resp"] = $this->transaction->status($request["TBK_TOKEN"]);
+            }
+            //Flujo normal
+            elseif ($request->exists("token_ws")) {
+                $resp = $this->transaction->commit($request["token_ws"]);
+                $view = 'webpay-deferred.commit';
+                $data = ["resp" => $resp, "token" => $request["token_ws"]];
+            }
+        } catch (\Exception $e) {
+            return view('error-page', ["error" => $e->getMessage()]);
         }
 
         return view($view, $data);
@@ -68,8 +76,9 @@ class WebpayPlusDeferredController extends Controller
                 'msg' => $e->getMessage(),
                 'code' => $e->getCode()
             );
-            return view('webpay-deferred.capture', ["resp" => $resp, "token" => $req["token"]]);
+            return view('error-page', ["error" => $e->getMessage()]);
         }
+
         return view('webpay-deferred.capture', ["resp" => $resp, "token" => $req["token"]]);
     }
 
@@ -79,19 +88,21 @@ class WebpayPlusDeferredController extends Controller
             $req = $request->except('_token');
             $resp = $this->transaction->refund($req["token"], $req["amount"]);
         } catch (\Exception $e) {
-            $resp = array(
-                'msg' => $e->getMessage(),
-                'code' => $e->getCode()
-            );
-            return view('webpay-deferred.refund', ["resp" => $resp]);
+            return view('error-page', ["error" => $e->getMessage()]);
         }
+
         return view('webpay-deferred.refund', ["resp" => $resp, 'token' => $req["token"]]);
     }
 
     public function status(Request $request)
     {
-        $token = $request["token"];
-        $resp = $this->transaction->status($token);
+        try {
+            $token = $request["token"];
+            $resp = $this->transaction->status($token);
+        } catch (\Exception $e) {
+            return view('error-page', ["error" => $e->getMessage()]);
+        }
+
         return view('webpay-deferred.status', ["resp" => $resp, "req" => $request]);
     }
 }
