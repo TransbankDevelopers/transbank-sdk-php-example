4cb26727da3c987231bc29cdaeb900ef0ff651e8
victor mendoza <ve.mendoza.blanco@gmail.com>
2025-04-01 16:25:32 -0300

    feat: add breadcrumb to webpay deferred views

--- resources/views/webpay-deferred/create.blade.php
+++ resources/views/webpay-deferred/create.blade.php
@@ -1,8 +1,18 @@
 @php
-$navigation = ['request' => 'Petición', 'response' => 'Respuesta', 'form' => 'Formulario', 'example' => 'Ejemplo'];
+    $navigation = ['request' => 'Petición', 'response' => 'Respuesta', 'form' => 'Formulario', 'example' => 'Ejemplo'];
 @endphp
 
 <x-layout active-link="Webpay Plus Diferido" :navigation="$navigation">
+    <div class="breadcrumbs-container">
+        <div class="breadcrumbs-items">
+
+            <a href="/">Inicio</a>
+            <img src={{ asset('images/t-arrow.svg') }} alt="t-arrow" width="24" height="24" />
+        </div>
+        <div class="breadcrumbs-items">
+            <a class="current-breadcrumb" href="/webpay-plus-diferido/create">Webpay Plus Diferido</a>
+        </div>
+    </div>
     <h1>Webpay Plus Diferido - Creación de transacción</h1>
     <p class="mb-32">
         En esta etapa, se procederá a la creación de una transacción con el fin de obtener un identificador
@@ -32,7 +42,8 @@ $navigation = ['request' => 'Petición', 'response' => 'Respuesta', 'form' => 'F
     <x-snippet :content="$respond" />
 
     <h2 id="form">Paso 3: Creación del formulario</h2>
-    <p class="mb-32">Utiliza estos datos de respuesta para redireccionar al usuario al formulario de pago al Tarjetahabiente.
+    <p class="mb-32">Utiliza estos datos de respuesta para redireccionar al usuario al formulario de pago al
+        Tarjetahabiente.
         Este formulario será la interfaz a través de la cual el usuario realizará su transacción.
     </p>
 
@@ -60,7 +71,8 @@ $navigation = ['request' => 'Petición', 'response' => 'Respuesta', 'form' => 'F
     <form action={{ $respond->url }} method="POST">
         <div class="tbk-card">
             <span class="tbk-card-title">Formulario de redirección</span>
-            <livewire:webpay-deferred-token :token="$respond->token" :amount="$request['amount']" :buyOrder="$request['buyOrder']" :sessionId="$request['sessionId']" :returnUrl="$request['returnUrl']" />
+            <livewire:webpay-deferred-token :token="$respond->token" :amount="$request['amount']" :buyOrder="$request['buyOrder']" :sessionId="$request['sessionId']"
+                :returnUrl="$request['returnUrl']" />
             <div class="tbk-card-footer">
                 <button class="tbk-button primary">PAGAR</button>
             </div>
