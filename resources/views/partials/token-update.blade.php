1a6071104848cef1cd55a1b80867f4390156ab4c
Matias Vasquez <m.vasquezac@gmail.com>
2025-04-03 10:57:18 -0300

    refactor: change const name

--- resources/views/partials/token-update.blade.php
+++ resources/views/partials/token-update.blade.php
@@ -2,8 +2,8 @@
 <script>
     document.addEventListener('livewire:init', () => {
         Livewire.on('token-updated', (data) => {
-            const spanList = document.querySelectorAll('.hljs-string');
-            spanList.forEach(span => {
+            const snippetSpanList = document.querySelectorAll('.hljs-string');
+            snippetSpanList.forEach(span => {
                 if (span.textContent.includes(data.oldToken)) {
                     span.textContent = span.textContent.replace(data.oldToken, data.token);
                 }
