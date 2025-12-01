e0a517d1470fc2a2dcd1382eac8340d3deeeb342
victor mendoza <victor.mendoza@continuumhq.com>
2025-12-01 13:35:14 -0300

    feat: add status view for tx complete

---
+++ resources/views/transaccion-completa/status.blade.php
@@ -0,0 +1,47 @@
+@php
+    $navigation = ['request' => 'Petición', 'response' => 'Respuesta'];
+@endphp
+
+<x-layout active-link="Transacción Completa" :navigation="$navigation">
+    <div class="breadcrumbs-container">
+        <div class="breadcrumbs-items">
+            <a href="/">Inicio</a>
+            <img src={{ asset('images/t-arrow.svg') }} alt="t-arrow" width="24" height="24" />
+        </div>
+        <div class="breadcrumbs-items">
+            <a href="/transaccion-completa">Webpay Transaccion Completa</a>
+            <img src={{ asset('images/t-arrow.svg') }} alt="t-arrow" width="24" height="24" />
+        </div>
+        <div class="breadcrumbs-items">
+            <a class="current-breadcrumb" href="/transaccion-completa/status">Status</a>
+        </div>
+
+    </div>
+
+    <h1>Transacción Completa - Estado de transacción</h1>
+    <p class="mb-32">En esta fase, tendrás la capacidad de solicitar el estado actual de una transacción hasta 7 días
+        después de su realización. Es importante destacar que no hay límite en la cantidad de solicitudes de este tipo
+        durante este período. Sin embargo, una vez transcurridos los 7 días, ya no podrás revisar el estado de la
+        transacción.</p>
+
+    <h2 id="request">Paso 1: Petición</h2>
+    <p class="mb-32">Para llevar a cabo la solicitud de estado, necesitarás el token correspondiente a la transacción
+        de la cual deseas obtener información. Utiliza este token para realizar una llamada a
+        TransaccionCompleta.Transaction.
+    </p>
+    <x-snippet>
+        use Transbank\Webpay\TransaccionCompleta\Transaction;
+        use Transbank\Webpay\Options;
+        //configuración de la transacción
+        $option = new Options(API_KEY, COMMERCE_CODE, Options::ENVIRONMENT_INTEGRATION);
+        $transaction = new Transaction($option);
+        $response = $resp = $transaction->status($token);
+    </x-snippet>
+
+    <h2 id="response">Paso 2: Respuesta</h2>
+    <p>Transbank responderá con la siguiente información. Asegúrate de guardar estos detalles; lo único que necesitas
+        validar es que el campo "response_code" sea igual a cero.</p>
+
+    <x-snippet :content="$respond" />
+
+</x-layout>
