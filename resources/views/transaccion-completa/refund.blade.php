4dd9c79eb949e3d2d008ba00eea266f4dfe993bb
victor mendoza <victor.mendoza@continuumhq.com>
2025-12-09 09:33:43 -0300

    fix: use correct link to refund

--- resources/views/transaccion-completa/refund.blade.php
+++ resources/views/transaccion-completa/refund.blade.php
@@ -13,7 +13,7 @@
             <img src={{ asset('images/t-arrow.svg') }} alt="t-arrow" width="24" height="24" />
         </div>
         <div class="breadcrumbs-items">
-            <a class="current-breadcrumb" href="/transaccion-completa/commit">Reversa</a>
+            <a class="current-breadcrumb" href="/transaccion-completa/refund">Reversa</a>
         </div>
 
     </div>
