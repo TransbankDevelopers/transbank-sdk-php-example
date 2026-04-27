51d61271d93c2eaee9c998773f5e34e780204728
victor mendoza <victor.mendoza@continuumhq.com>
2026-04-27 16:14:49 -0300

    feat: add refund view for Oneclick Mall Promociones

---
+++ resources/views/promotions-oneclick-mall/refund.blade.php
@@ -0,0 +1,84 @@
+@php
+    $navigation = ['request' => 'Petición', 'response' => 'Respuesta'];
+@endphp
+
+<x-layout active-link="Oneclick Mall Promociones" :navigation="$navigation">
+    <div class="breadcrumbs-container">
+        <div class="breadcrumbs-items">
+            <a href="/">Inicio</a>
+            <img src={{ asset('images/t-arrow.svg') }} alt="t-arrow" width="24" height="24" />
+        </div>
+        <div class="breadcrumbs-items">
+            <a href="/promotions-oneclick-mall">Oneclick Mall Promociones</a>
+            <img src={{ asset('images/t-arrow.svg') }} alt="t-arrow" width="24" height="24" />
+        </div>
+        <div class="breadcrumbs-items">
+            <a class="current-breadcrumb" href="#">Reembolsar</a>
+        </div>
+    </div>
+
+    <h1>Oneclick Mall Promociones - Reembolsar</h1>
+    <p class="mb-32">
+        En esta etapa, tienes la opción de solicitar el reembolso del monto al titular de la tarjeta.
+        Dependiendo del monto y el tiempo transcurrido desde la transacción, este proceso podría resultar en una Reversa
+        o Anulación, dependiendo de ciertas condiciones (Reversa en las primeras 3 horas de la autorización, anulación
+        posterior a eso), o una Anulación parcial si el monto es menor al total.
+        Las anulaciones parciales para tarjetas débito y prepago no están soportadas.
+    </p>
+
+    <h2 id="request">Paso 1 - Petición:</h2>
+    <p class="mb-16">
+        Para realizar el reembolso, necesitarás la siguiente información:
+    </p>
+
+    <ul class="mb-32">
+        <li>Orden de compra de la transacción.</li>
+        <li>Monto que deseas reversar.</li>
+        <li>Código de comercio (tienda)</li>
+        <li>Orden de compra del detalle de la transacción (tienda).</li>
+    </ul>
+
+    <p class="mb-32">
+        Las transacciones de Webpay se pueden anular o reversar dadas algunas condiciones. Para cualquiera de éstas
+        operaciones se utiliza el mismo servicio web que discernirá si se realizará una reversa o una anulación.
+
+        Para poder ejecutar una reversa ésta debe ser realizada antes de las 3 horas de efectuada la confirmación por el
+        monto total y en compras con tarjeta de crédito, débito o prepago.
+
+        Una vez pasadas las tres horas, siempre se ejecutará una anulación.
+    </p>
+    <ul class="mb-32">
+        <li>
+            En transacciones con tarjeta de débito o prepago solo es posible anular por el monto total.
+        </li>
+        <li>
+            En transacciones con tarjeta de crédito puedes anular por cualquier monto igual o menor al total de la
+            compra.
+        </li>
+        <li>
+            No se pueden hacer Anulaciones parciales de compras con cuotas.
+        </li>
+    </ul>
+
+    <p class="mb-32">
+        En <a href="https://www.transbankdevelopers.cl/producto/webpay#anulaciones-y-reversas" class="tbk-link">este
+            link
+        </a> podrás ver mayor información sobre las condiciones y casos para anular o reversar transacciones.
+    </p>
+
+    <x-snippet>
+        $resp = $mallTransaction->refund($buyOrder, $childCommerceCode, $childBuyOrder, $amount);
+    </x-snippet>
+
+    <h2 id="response">Paso 2 - Respuesta:</h2>
+    <p class="mb-32">
+        Transbank responderá con el resultado de la reversa o anulación. Analiza esta respuesta para confirmar que el
+        reembolso se ha procesado correctamente.
+    </p>
+
+    <x-snippet :content="$resp" />
+
+    <a href={{ route('promotions-oneclick-mall.status', ['buyOrder' => $buyOrder]) }} class="tbk-button primary mb-32">CONSULTAR
+        ESTADO</a>
+
+</x-layout>
