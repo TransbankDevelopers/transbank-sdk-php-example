49afba4f35c4b986badcd42d37d895bf48e40c32
victor mendoza <victor.mendoza@continuumhq.com>
2026-04-29 14:45:07 -0300

    feat: update PromotionsOneclickToken to use configuration for API key and commerce code

--- app/Livewire/PromotionsOneclickToken.php
+++ app/Livewire/PromotionsOneclickToken.php
@@ -10,8 +10,8 @@ class PromotionsOneclickToken extends OneclickToken
     protected function getProduct()
     {
         $option = new Options(
-            'd8f06df8-39c7-4f01-8e74-b383c19ae836',
-            '597060000001',
+            config('app.transbank.oneclick_promotions_api_key'),
+            config('app.transbank.oneclick_promotions_cc'),
             Options::ENVIRONMENT_INTEGRATION
         );
 
